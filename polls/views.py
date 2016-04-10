9a655f3d1d5d7f646d442c034e815189258ef207
Alexander Sopov <sopovalexander@gmail.com>
2016-04-10 02:46:25 +0200

    added test units

--- polls/views.py
+++ polls/views.py
@@ -2,9 +2,11 @@ from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponseRedirect, HttpResponse
 from django.core.urlresolvers import reverse
 from django.views import generic
+from django.utils import timezone
 
 from .models import Choice, Question
 # Create your views here.
+
 
 class IndexView(generic.ListView):
     template_name = 'polls/index.html'
@@ -12,7 +14,7 @@ class IndexView(generic.ListView):
 
     def get_queryset(self):
         """return the last five published questions."""
-        return Question.objects.order_by('-pub_date')[:5]
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
 
 
 class DetailView(generic.DetailView):
