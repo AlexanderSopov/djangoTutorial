636c1e529cc256f446f473dccc3dda8f2ad98912
Alexander Sopov <sopovalexander@gmail.com>
2016-04-11 00:38:11 +0200

    app functionality complete

--- polls/views.py
+++ polls/views.py
@@ -20,6 +20,9 @@ class IndexView(generic.ListView):
 class DetailView(generic.DetailView):
     model = Question
     template_name = "polls/detail.html"
+    def get_queryset(self):
+        """ Exclude any question in the future"""
+        return Question.objects.filter(pub_date__lte=timezone.now())
 
 class ResultsView(generic.DetailView):
     model = Question
@@ -44,3 +47,47 @@ def vote(request, question_id):
         # user hits the Back button.
 
     return HttpResponseRedirect(reverse("polls:results",args=(question.id,)))
+
+def create(request):
+    #render a template
+    print("got to create")
+    return render(request, "polls/create.html",{})
+
+def createPoll(request):
+    #render a template
+
+    try:
+        qText=request.POST["question"]
+        choices = request.POST.getlist("choice")
+        print(choices)
+
+    except(KeyError):
+        print("something went wrong at createPoll")
+        return render(request, "polls/create.html",{
+            "error_message": "You didn't type a question or any choice in.",
+        })
+
+
+    else:
+        if len(qText)>0:
+            if (len(choices[0]) > 0 and len(choices[1]) > 0):
+                q = Question.objects.create(question_text=qText, pub_date=timezone.now())
+                q.save()
+                for choice in choices:
+                    if len(choice) > 0:
+                        q.choice_set.create(choice_text=choice, votes=0)
+                q.save()
+            else:
+                return render(request, "polls/create.html",{
+                "error_message": "You didn't type ta least 2 choices in.",})
+        else:
+            return render(request, "polls/create.html",{
+            "error_message": "You didn't type a question in.",})
+
+
+
+        # Always return an HttpResonseRedirect after sucessfully dealing
+        # with POST data. This prevents data from being posted twice if a
+        # user hits the Back button.
+
+    return HttpResponseRedirect(reverse("polls:index"),)
